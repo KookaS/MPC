6b4db1ff4aff94f82d3cb6760a645ff87e4694ab
MatthieuJacobs <matthieu.jacobs@student.kuleuven.be>
2020-12-10 10:49:25 +0100

    Just testing git in matlab

--- src/main.m
+++ src/main.m
@@ -1,7 +1,7 @@
 % load('src/handout/Quad.m')
 addpath('/home/olivier/MATLAB/casadi-linux-matlabR2014b-v3.5.5')
 import casadi.*
-
+% Just testing git
 quad = Quad();
 Tf = 1.0; % Time to simulate for
 x0 = zeros(12,1);
